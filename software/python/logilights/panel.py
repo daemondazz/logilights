b122b65aa7199adc3d13a21507d8f5a4b1c1e791
Darryl Ross <darryl@afoyi.com>
2014-12-20 00:30:24 +0000

    Add initial python code for driving the panel and scrolling text.

---
+++ software/python/logilights/panel.py
@@ -0,0 +1,118 @@
+import logi
+from struct import pack, unpack
+from random import shuffle
+from time import sleep, time
+
+
+DISPLAY_WIDTH = 94
+DISPLAY_HEIGHT = 32
+
+FPGA_PANEL_ADDR_REG = 0x0008
+FPGA_PANEL_DATA_REG = 0x0009
+FPGA_PANEL_BUFFER_REG = 0x000a
+
+FPGA_BUFFER1_SELECT = 0x0000
+FPGA_BUFFER2_SELECT = 0x0001
+
+FPGA_BUFFER1_OFFSET = 0x0000
+FPGA_BUFFER2_OFFSET = 0x2000
+
+# 8 primary colours
+COLOUR_ARRAY = {
+    'BLACK'  : 0x0000,
+    'WHITE'  : 0xfff,
+    'RED'    : 0xf00,
+    'GREEN'  : 0x0f0,
+    'BLUE'   : 0x00f,
+    'AQUA'   : 0x0ff,
+    'PURPLE' : 0xf0f,
+    'YELLOW' : 0xff0
+}
+COLOURS = COLOUR_ARRAY.values()
+
+
+class Panel(object):
+    def __init__(self):
+        self.active_buffer = 0
+        self.pixel_buffer = []
+
+    def blank_display(self):
+        self.pixel_buffer = []
+
+        # Fill it with zeros
+        for x in xrange(DISPLAY_HEIGHT):
+            self.pixel_buffer.append([0] * DISPLAY_WIDTH)
+
+        # Write it
+        self.write_levels()
+
+    def write_levels(self):
+
+        # Switch the active write buffer
+        if self.active_buffer == 0:
+            base = FPGA_BUFFER1_OFFSET
+        else:
+            base = FPGA_BUFFER2_OFFSET
+
+        # Write the data
+        for row in xrange(DISPLAY_HEIGHT):
+            self.write_word(FPGA_PANEL_ADDR_REG, (base + (128 * row)))
+            for col in xrange(DISPLAY_WIDTH):
+                self.write_word(FPGA_PANEL_DATA_REG,
+                                self.pixel_buffer[row][col])
+
+        # Switch the active display buffer 
+        if self.active_buffer == 0:
+            self.write_word(FPGA_PANEL_BUFFER_REG, 0x0000)
+            self.active_buffer = 1
+        else:
+            self.write_word(FPGA_PANEL_BUFFER_REG, 0x0001)
+            self.active_buffer = 0
+
+    def write_word(self, addr, word):
+        logi.logiWrite(addr, self._fmt_tuple(word))
+
+    def _fmt_tuple(self, i):
+        i = (i >> 8) + ((i & 0xff) << 8)
+        hi_bits = i >> 8
+        lo_bits = i & 0xff
+        return (hi_bits, lo_bits)
+
+
+class TestPattern1(Panel):
+    def first_frame(self):
+        self.blank_display()
+
+    def next_frame(self):
+        global COLOURS
+        self.pixel_buffer = []
+        COLOURS.append(COLOURS.pop(0))
+        for x in xrange(DISPLAY_HEIGHT):
+            row = [COLOURS[x % len(COLOURS)]] * DISPLAY_WIDTH
+            self.pixel_buffer.append(row)
+        self.write_levels()
+
+
+class TestPattern2(Panel):
+    def first_frame(self):
+        self.blank_display()
+
+    def next_frame(self):
+        global COLOURS
+        self.pixel_buffer = []
+        shuffle(COLOURS)
+        for x in xrange(DISPLAY_HEIGHT):
+            row = [COLOURS[x % len(COLOURS)]] * DISPLAY_WIDTH
+            self.pixel_buffer.append(row)
+        self.write_levels()
+
+
+if __name__ == '__main__':
+    def run60(p):
+        p.first_frame()
+        start = time()
+        while time() < start + 60:
+            p.next_frame()
+            sleep(0.025)
+    run60(TestPattern1())
+    run60(TestPattern2())
---
+++ software/python/scrolling_text.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+from time import sleep
+
+from logilights.font import Font
+from logilights.panel import COLOUR_ARRAY, DISPLAY_HEIGHT, DISPLAY_WIDTH
+from logilights.panel import Panel
+
+
+class ScrollingText(Panel):
+    def __init__(self, colour, font_file, text, *args, **kwargs):
+        super(ScrollingText, self).__init__(*args, **kwargs)
+        self.colour = COLOUR_ARRAY[colour]
+        self.fnt = Font(font_file, 28)
+        self.offset = 0
+
+        # Work out how many spaces to add before and after for pre/post roll
+        num_spaces = (DISPLAY_WIDTH / self.fnt.render_text(' ').width) + 1
+
+        # Pre-render the text
+        self.data = self.fnt.render_text(' ' * num_spaces + text + ' ' * num_spaces)
+
+    def first_frame(self):
+        self.blank_display()
+        self.pixel_buffer = self.get_viewport()
+        self.write_levels()
+
+    def next_frame(self):
+        if not self.slide_viewport(1):
+            sys.exit(0)
+        self.pixel_buffer = self.get_viewport()
+        self.write_levels()
+
+    def get_viewport(self):
+        gap_above = int((DISPLAY_HEIGHT - self.data.height) / 2)
+        gap_below = DISPLAY_HEIGHT - gap_above - self.data.height
+        tmp_buffer = []
+        for row in xrange(gap_above):
+            tmp_buffer.append([0] * DISPLAY_WIDTH)
+        for row in xrange(self.data.height):
+            tmp_buffer.append([0] * DISPLAY_WIDTH)
+            row_start = row * self.data.width
+            row_end = row_start + DISPLAY_WIDTH
+            pixels = self.data.pixels[self.offset+row_start:self.offset+row_end]
+            for col in xrange(DISPLAY_WIDTH):
+                if pixels[col]:
+                    tmp_buffer[row+gap_above][col] = self.colour
+        for row in xrange(gap_below):
+            tmp_buffer.append([0] * DISPLAY_WIDTH)
+        return tmp_buffer
+
+    def slide_viewport(self, amount=1):
+        if self.offset == self.data.width - DISPLAY_WIDTH:
+            return False
+        self.offset += amount
+        return True
+
+
+if __name__ == '__main__':
+    p = ScrollingText(sys.argv[1], sys.argv[2], ' '.join(sys.argv[3:]))
+    p.first_frame()
+    while True:
+        p.next_frame()
