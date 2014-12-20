4b84abcfb451e41266e2c0ea2e80ffc695fde6e3
Darryl Ross <darryl@afoyi.com>
2014-12-20 13:28:00 +0000

    Add run files for both scripts.

---
+++ software/python/run_merry_christmas_twoline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while /bin/true; do
+	./two_line_text.py fonts/HARNGTON.TTF YELLOW Seasons YELLOW Greetings; sleep 3;
+	./two_line_text.py fonts/OLDENGL.TTF RED Merry GREEN Christmas; sleep 3;
+	./two_line_text.py fonts/COOPBL.TTF BLUE Joyeux YELLOW Noel; sleep 3;
+	./two_line_text.py fonts/yataghan.ttf AQUA Feliz PURPLE Navidad; sleep 3;
+	./two_line_text.py fonts/HARNGTON.TTF GREEN "Sheng Dan" RED "Kuai Le"; sleep 3;
+	./two_line_text.py fonts/RAVIE.TTF PURPLE Wesolych BLUE Swiat; sleep 3;
+done
