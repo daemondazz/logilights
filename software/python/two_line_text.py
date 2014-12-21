8a74dbbd191d71d440b7ed92ab91e83f2d43e041
Darryl Ross <darryl@afoyi.com>
2014-12-21 13:16:09 +1100

    Update two_line_text script to includes messages at top of file and to also support pre-rendering to reduce rendering time and memory requirements for instantiating FreeType instance every frame. Also remove two_line_text run file as the python script can now be called directly from anywhere.

--- software/python/two_line_text.py
+++ software/python/two_line_text.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
+
+MESSAGES = [
+    ['fonts/HARNGTON.TTF', 'YELLOW', 'Seasons', 'YELLOW', 'Greetings', None],
+    ['fonts/OLDENGL.TTF', 'RED', 'Merry', 'GREEN', 'Christmas', None],
+    ['fonts/COOPBL.TTF', 'BLUE', 'Joyeux', 'YELLOW', 'Noel', None],
+    ['fonts/yataghan.ttf', 'AQUA', 'Feliz', 'PURPLE', 'Navidad', None],
+    ['fonts/HARNGTON.TTF', 'GREEN', 'Sheng Dan', 'RED', 'Kuai Le', None],
+    ['fonts/RAVIE.TTF', 'PURPLE', 'Wesolych', 'BLUE', 'Swiat', None],
+]
+
+
 import sys
+import os, os.path
 from time import sleep
 
 from logilights.font import Font
@@ -9,12 +21,26 @@ from logilights.panel import Panel
 
 
 class TwoLineText(Panel):
-    def __init__(self, font_file, colour1, text1, colour2, text2, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(TwoLineText, self).__init__(*args, **kwargs)
 
+    def render_message(self, font_file, colour1, text1, colour2, text2, prerendered):
+        global MESSAGES
+
+        # If the frame has been pre-rendered, just show it
+        if prerendered:
+            self.pixel_buffer = prerendered
+            self.write_levels()
+            return True
+
+        # Blank the screen
         self.pixel_buffer = []
         for row in xrange(DISPLAY_HEIGHT):
             self.pixel_buffer.append([0]*DISPLAY_WIDTH)
+
+        # Determine the location of the font file, so we don't have to specify
+        # the full path above
+        font_file = os.path.join(os.path.dirname(__file__), font_file)
 
         # Determine best font size to use
         font1 = self.calculate_font(font_file, text1, DISPLAY_HEIGHT/2)
@@ -26,6 +52,9 @@ class TwoLineText(Panel):
 
         # Render line 2
         self.render_line(16, COLOUR_ARRAY.get(colour2, 'RED'), text2)
+
+        # Save the current frame to the prerender spot in the first MESSAGE
+        MESSAGES[0][5] = self.pixel_buffer
 
         # Write Levels
         self.write_levels()
@@ -52,8 +81,8 @@ class TwoLineText(Panel):
         
 
 if __name__ == '__main__':
-    TwoLineText(sys.argv[1],
-                sys.argv[2],
-                sys.argv[3],
-                sys.argv[4],
-                sys.argv[5])
+    p = TwoLineText()
+    while True:
+        p.render_message(*MESSAGES[0])
+        MESSAGES.append(MESSAGES.pop(0))
+        sleep(5)
