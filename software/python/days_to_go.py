bda8cbfb1ea92b396ad49a5d3553e8c41562c33b
Darryl Ross <darryl@afoyi.com>
2014-12-21 19:28:54 +1100

    Add current date and time to panel.

--- software/python/days_to_go.py
+++ software/python/days_to_go.py
@@ -5,6 +5,7 @@ EVENT_NAME = ['SLEEP', 'LEFT']
 
 ROWS_COUNTER = 1
 ROWS_EVENT = 2
+ROWS_DATETIME = 4
 
 import sys
 import os, os.path
@@ -18,6 +19,8 @@ from logilights.panel import Panel
 
 font_file = os.path.join(os.path.dirname(__file__),
                          'fonts', 'special-elite.ttf')
+font_file2 = os.path.join(os.path.dirname(__file__),
+                         'fonts', '5x7-practical.ttf')
 
 
 class DaysToGoText(Panel):
@@ -25,6 +28,7 @@ class DaysToGoText(Panel):
         super(DaysToGoText, self).__init__(*args, **kwargs)
         self.font_cache = {}
         self.font_file = font_file
+        self.show_colon = True
 
     def render_panel(self):
 
@@ -35,6 +39,12 @@ class DaysToGoText(Panel):
 
         # Calculate how many days remaining until the target date
         days_remaining = str((datetime(*TARGET_DATE) - datetime.today()).days+1)
+        current_date = datetime.now().strftime('%d/%m')
+        if self.show_colon:
+            current_time = datetime.now().strftime('%H:%M')
+        else:
+            current_time = datetime.now().strftime('%H.%M')
+        self.show_colon = not self.show_colon
 
         # Pluralise firt word of event name
         if int(days_remaining) != 1 and EVENT_NAME[0][-1] != 'S':
@@ -44,6 +54,8 @@ class DaysToGoText(Panel):
         self.calculate_font(days_remaining, key='counter', rows=ROWS_COUNTER)
         for word in EVENT_NAME:
             self.calculate_font(word, key='event_name', rows=ROWS_EVENT)
+        self.calculate_font(current_time, key='date_time', rows=1, size=16, font_file=font_file2)
+        self.calculate_font(current_time, key='date_time', rows=1, size=16, font_file=font_file2)
 
         # Indent from the left edge of the screen
         h_offset = 5
@@ -57,6 +69,10 @@ class DaysToGoText(Panel):
         for word in EVENT_NAME:
             rendered = self.render_text(word, h_offset, v_offset, 'event_name', COLOUR_ARRAY['GREEN'], ROWS_EVENT)
             v_offset += rendered.height + 1
+
+        # Render the current date time
+        rendered1 = self.render_text(current_time, -1, -1, 'date_time', COLOUR_ARRAY['YELLOW'], ROWS_DATETIME)
+        rendered2 = self.render_text(current_date, -1, -1-rendered1.height-1, 'date_time', COLOUR_ARRAY['YELLOW'], ROWS_DATETIME)
 
         # Write Levels
         self.write_levels()
