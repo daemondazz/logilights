f8e2b477c02a5364d68a1e217af2f68a13f9f042
Darryl Ross <darryl@afoyi.com>
2014-12-24 20:01:50 +1100

    Add a bash script for being smart about what we put on the display based on the current date and time.

---
+++ run_messages.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+cleanup() {
+	echo "running cleanup"
+	killall timeout
+	exit
+}
+
+seconds_till() {
+	local current_ts=$( date +%s )
+	local end_ts=$1
+	TIME=$(( $end_ts - $current_ts))
+	# Hack for 5 minutes offset from quarter hour chime
+	# If we've already passed the 10 minutes offset from the last quarter hour
+	# chime, skip to the next one
+	if [[ ${TIME} -le 0 ]]; then
+		TIME=$(( $TIME + 900))
+	fi
+	echo $TIME
+}
+
+# Make sure we cleanup children
+trap cleanup SIGINT
+trap cleanup SIGKILL
+trap cleanup SIGTERM
+
+TIMEOUT="timeout --preserve-status -k 5"
+
+# Keep running forever
+while /bin/true; do
+
+	# Some helper variables
+	eval $( date +YEAR=%Y\;MONTH=%m\;DAY=%d\;HOUR=%H\;MIN=%M )
+
+	# If it's Christmas day, just run our Merry Christmas panel until midnight
+	if [[ "${MONTH}-${DAY}" == "12-25" ]]; then
+		$TIMEOUT $( seconds_till $( date -d "tomorrow 00:00:00" +%s )) \
+		         software/python/two_line_text.py merry_christmas_today.json
+
+	# If we are in December and it's before Christmas, show the seasons greetings
+	# and sleeps remaining panels
+	elif [[ "${MONTH}" -eq "12" && "${DAY}" -lt "25" ]]; then
+
+		# Between ww:55 and xx:10 show sleeps left
+		# Between xx:10 and xx:25 show seasons greetings
+		# Between xx:25 and xx:40 show sleeps left
+		# Between xx:40 and xx:55 show seasons greetings
+		MIDNIGHT=$( date -d "00:00:00" +%s )
+		NEXT_ROLLOVER=$(( $( date +%s) - ($(date +%s) % (15 * 60)) + 900 ))
+
+#		echo "NOW=$(date +%s), NEXT=${NEXT_ROLLOVER}"
+
+		if [[ "$(( ((${NEXT_ROLLOVER} - ${MIDNIGHT}) / 900) % 2 ))" -eq "0" ]]; then
+			$TIMEOUT $( seconds_till ${NEXT_ROLLOVER}) \
+			         software/python/days_to_go.py ${YEAR}-12-25
+		else
+			$TIMEOUT $( seconds_till ${NEXT_ROLLOVER}) \
+			         software/python/two_line_text.py merry_christmas_languages.json
+		fi
+
+	# If we are in December and it's after Christmas, just show the seasons
+	# greetings panels
+	elif [ "${MONTH}" -eq "12" && "${DAY}" -gt "25" ]; then
+		$TIMEOUT $( seconds_till $( date -d "tomorrow 00:00:00" +%s )) \
+		         software/python/two_line_text.py merry_christmas_languages.json
+
+	# Otherwise, if it's not December, show current time and temperature panel
+	else
+		$TIMEOUT $( seconds_till $( date -d "tomorrow 00:00:00" +%s )) \
+		         software/python/time_and_temperature.py
+
+	fi
+	sleep 1
+done
